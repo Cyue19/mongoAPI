b5589feede8d7464c1565f9d515893a0c79efab5
Nadim El-Jaroudi <nje2494@gmail.com>
2021-10-29 14:49:27 -0400

    Added New API Calls

--- Routes/files.js
+++ Routes/files.js
@@ -5,14 +5,40 @@ const File = require("../Models/file");
 //Get all files
 router.get("/", async (req, res) => {
     try {
-        const files = await File.find();
+        const files = await File.find().sort([['lastModified', -1]]);
         res.json(files);
     } catch (err) {
         res.status(500).json({message: err.message});
     }
 });
 
-router.get("/:fileName", async (req, res) => {
+router.get("/last", async (req, res) => {
+    try {
+        const files = await File.findOne({}, null, {sort : { lastModified: -1 }});
+        res.json(files);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+});
+
+router.get("/byDate", async (req, res) => {
+    try {
+        const files = await File.aggregate([{ "$group":         {
+            _id: {
+                $dateToString: {
+                    date: {$dateFromString: {dateString: '$lastModified'}},
+                    "format": "%Y-%m-%d"
+                }
+            }, 
+            count: { $sum:1 }
+        }}, {"$sort": {_id: -1 }}]);
+        res.json(files);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+});
+
+router.get("/search/:fileName", async (req, res) => {
     try {
         const files = await File.find({fileName: req.params.fileName});
         res.json(files);
@@ -20,5 +46,6 @@ router.get("/:fileName", async (req, res) => {
         res.json({message: err.message});
     }
 });
+
 
 module.exports = router;
